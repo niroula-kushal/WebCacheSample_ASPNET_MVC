833987ff1dde336a70a1e342dcfbbf71ec98eea3
Kushal Niroula <niroulakushal31@gmail.com>
2021-12-15 19:52:37 +0545

    Feat: Initial Commit

---
+++ ExpensiveDataProvider.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Runtime.Caching;
+
+namespace WebCacheSample
+{
+    public class ExpensiveDataProvider
+    {
+        private const string Key = "expensive_key";
+
+        public DateTime GetDate()
+        {
+            var factory = new Lazy<DateTime>(() => CalculateDate());
+            var date = (Lazy<DateTime>) MemoryCache.Default.AddOrGetExisting(Key, factory, new CacheItemPolicy()
+            {
+                SlidingExpiration = TimeSpan.FromDays(7)
+            });
+            return (date ?? factory).Value;
+        }
+
+        public DateTime CalculateDate() => DateTime.Now;
+    }
+}
